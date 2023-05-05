9e35eb9d0ed98a3adf58f42659a4eb1921bde70b
Guilherme Salvador <guilhermesalvador@hotmail.com>
2023-05-05 20:20:45 -0300

    02_criando_classes_camada_service

--- src/main/kotlin/me/dio/creditapplicationsystem/CreditRepository.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/CreditRepository.kt
@@ -2,7 +2,14 @@ package me.dio.creditapplicationsystem
 
 import me.dio.creditapplicationsystem.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CreditRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long>{
+
+    fun findByCreditCode(creditCode: java.util.UUID): Credit?
+
+    @Query(value = "SELECT * FROM CREDIT WHERE Customer_Id = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
+}
---
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/impl/CustomerService.kt
@@ -0,0 +1,23 @@
+package me.dio.creditapplicationsystem.service.impl
+
+import me.dio.creditapplicationsystem.CustomerRepository
+import me.dio.creditapplicationsystem.entity.Customer
+import me.dio.creditapplicationsystem.service.ICustomerService
+import org.springframework.stereotype.Service
+import java.lang.RuntimeException
+
+@Service
+class CustomerService(
+    private val customerRepository: CustomerRepository): ICustomerService {
+
+    override fun save(customer: Customer): Customer = this.customerRepository.save(customer)
+
+
+    override fun findById(id: Long): Customer = this.customerRepository.findById(id)
+        .orElseThrow{throw RuntimeException("Id $id not found")}
+
+
+    override fun delete(id: Long) = this.customerRepository.deleteById(id)
+
+
+}
